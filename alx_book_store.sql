9abe3fac2bee8c918426190fdab11fb9b152b26c
Baki78 <bakiget78@gmail.com>
2024-07-28 13:20:10 +0300

    5th

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 use alx_book_store;
 CREATE TABLE Authors (author_id int primary key, author_name VARCHAR(215));
+CREATE TABLE Books (book_id int primary key,author_id INT, title VARCHAR(130), price DOUBLE, publication_date DATE, FOREIGN KEY (author_id) REFERENCES Authors(author_id));
+
 SHOW TABLES;
 SHOW columns from Books;
