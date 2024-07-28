c8c1d3dce538e9a7c8d7f3b16a641fa89904cb80
Baki78 <bakiget78@gmail.com>
2024-07-28 18:56:42 +0300

    21th

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,14 @@
 USE alx_book_store;
-SELECT * from alx_book_store;
+
+SELECT 
+    COLUMN_NAME AS 'Column Name', 
+    COLUMN_TYPE AS 'Column Type', 
+    IS_NULLABLE AS 'Is Nullable', 
+    COLUMN_KEY AS 'Key', 
+    COLUMN_DEFAULT AS 'Default', 
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
